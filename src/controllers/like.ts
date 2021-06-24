1c78ccb43ffeae0788062ec7de8aaa17c3c17f06
padrocha <p.roch@outlook.es>
2021-06-24 06:03:14 -0500

    toogle fix

--- src/controllers/like.ts
+++ src/controllers/like.ts
@@ -2,33 +2,33 @@ import { Request, Response } from "express";
 import { FilterQuery, isValidObjectId, QueryOptions, UpdateQuery } from "mongoose";
 import { LikeModel, ILike } from "../models/like";
 
-export function toogleLike({ params, body }: Request, res: Response) {
-    if (!isValidObjectId(params?.post) || !isValidObjectId(params?.user))
-        return res.status(400).send({ message: 'Client has not sent params' });
+export function toogleLike({ query }: Request, res: Response) {
+    if (!isValidObjectId(query?.post) || !isValidObjectId(query?.user))
+        return res.status(400).send({ message: 'Client has not sent query' });
 
-    const post = params.post as any;
-    const user = params.user as any;
-    const query: FilterQuery<ILike> = { post, user, };
-    const update: UpdateQuery<ILike> = { post, user, toogle: !body?.toogle, }
+    const post = query.post as any;
+    const user = query.user as any;
+    const filter: FilterQuery<ILike> = { post, user, };
+    const update: UpdateQuery<ILike> = { post, user, toogle: !query?.toogle, }
     const options: QueryOptions = { upsert: true, }
-    LikeModel.findOneAndUpdate(query, update, options)
+    LikeModel.findOneAndUpdate(filter, update, options)
         .select(['toogle'])
         .exec((err, LikeUpdated) => {
-            if (err) return res.status(409).send({ message: 'Internal error, probably error with params' });
+            if (err) return res.status(409).send({ message: 'Internal error, probably error with query' });
             if (!LikeUpdated) return res.status(200).send({ data: { toogle: false } });
             return res.status(200).send({ data: LikeUpdated });
         });
 }
 
-export function getLike({ params }: Request, res: Response) {
-    if (!isValidObjectId(params?.post) || !isValidObjectId(params?.user))
-        return res.status(400).send({ message: 'Client has not sent params' });
+export function getLike({ query }: Request, res: Response) {
+    if (!isValidObjectId(query?.post) || !isValidObjectId(query?.user))
+        return res.status(400).send({ message: 'Client has not sent query' });
 
     LikeModel.findOne()
-        .where({ post: params.post, user: params.user })
+        .where({ post: query.post, user: query.user })
         .select(['toogle'])
         .exec((err, Like) => {
-            if (err) return res.status(409).send({ message: 'Internal error, probably error with params' });
+            if (err) return res.status(409).send({ message: 'Internal error, probably error with query' });
             if (!Like) return res.status(200).send({ data: { toogle: false } });
             return res.status(200).send({ data: Like });
         });
